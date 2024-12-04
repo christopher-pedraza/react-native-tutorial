c07dc4b8cdcb63ff27433ed6fdcfb7261ac3157d
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2024-12-04 13:06:52 -0600

    Agregar tabs para navegacion

--- react-native-tutorial/app/(tabs)/about.tsx
+++ react-native-tutorial/app/(tabs)/about.tsx
@@ -1,5 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
 
+export default function AboutScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>About screen</Text>
+        </View>
+    );
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -11,13 +19,3 @@ const styles = StyleSheet.create({
         color: "#fff",
     },
 });
-
-const AboutScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>About screen</Text>
-        </View>
-    );
-};
-
-export default AboutScreen;
